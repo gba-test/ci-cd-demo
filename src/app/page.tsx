8dda94058ee69e0b4fd38be3b48cb3c3ecb2c717
guillaumeb0 <guillaume.bakaga@edu.itescia.fr>
2023-10-22 16:08:31 +0200

    new text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
     <main className="main">
       <div>
         <h2>Hello World</h2>
-        <p>New text!!!</p>
+        <p>New text!?</p>
       </div>
       <Link href="/about">About</Link>
       <br />
