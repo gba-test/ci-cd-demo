174c85c53da3a4fcabda42663f796c2a95f915db
guillaumeb0 <guillaume.bakaga@edu.itescia.fr>
2023-10-22 11:52:56 +0200

    test staging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default async function Home() {
     <main className="main">
       <div>
         <h2>Hello World</h2>
+        <p>New text!!!</p>
       </div>
       <Link href="/about">About</Link>
       <br />
